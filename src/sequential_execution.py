f8dfd84f53cea8101d943f259fc8d5f9838cafb8
Joy Anne Dela Cruz <60301959@udst.edu.qa>
2025-02-02 12:16:25 +0300

    Added serial case

---
+++ src/sequential_execution.py
@@ -0,0 +1,12 @@
+import time
+from add_random_numbers import add_random_numbers
+from join_random_letters import join_random_letters
+
+def sequential_case():
+    total_start_time = time.time()
+    join_random_letters()
+    add_random_numbers()
+    total_end_time = time.time()
+    print(f"Serial case time taken:  {total_end_time - total_start_time} seconds")
+
+sequential_case()
