f10d06ee1241d49127065672dc11e0736df3ba09
Joy Anne Dela Cruz <60301959@udst.edu.qa>
2025-02-02 12:25:16 +0300

    Added multiprocessing case

---
+++ src/multiprocessing_case.py
@@ -0,0 +1,17 @@
+import time
+import multiprocessing
+from add_random_numbers import add_random_numbers
+from join_random_letters import join_random_letters
+
+def multiprocessing_case():
+    process_letters = multiprocessing.Process(target=join_random_letters)
+    process_numbers = multiprocessing.Process(target=add_random_numbers)
+
+    total_start_time = time.time()
+    process_letters.start()
+    process_numbers.start()
+    process_letters.join()
+    process_numbers.join()
+    total_end_time = time.time()
+
+    print(f"Multiprocessing case time taken:  {total_end_time - total_start_time} seconds")
