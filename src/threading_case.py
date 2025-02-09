b7557ca0e1a01b5053e5758bc3af516dfb8abb5f
Joy Anne Dela Cruz <60301959@udst.edu.qa>
2025-02-09 13:16:57 +0300

    /bin/python3

--- src/threading_case.py
+++ src/threading_case.py
@@ -1,31 +1,40 @@
 import time
 import threading
 from .add_random_numbers import add_random_numbers
-from .join_random_letters import join_random_letters
 
-def threading_case(n):
-    """
-    Executes two functions in parallel using threading.
+def wrapper_add_random_numbers(start, end, result_list):
+        result_list.append(add_random_numbers(start, end))
 
-    This function runs `join_random_letters(n)` and `add_random_numbers(n)`
-    concurrently using Python threads.
-
-    Parameters:
-    n (int): The input size for both functions.
-
-    Returns:
-    float: Total execution time in milliseconds.
-    """
-    thread_letters = threading.Thread(target=join_random_letters, args=(n,))
-    thread_numbers = threading.Thread(target=add_random_numbers, args=(n,))
-
+def threading_case(num_threads=2, num_numbers=1000):
+    print(f"\nStarting all {num_threads} threads...")
     total_start_time = time.time()
-    thread_letters.start()
-    thread_numbers.start()
-    thread_letters.join()
-    thread_numbers.join()
-    total_end_time = time.time()
 
+    chunk_size = num_numbers // num_threads
+    threads = []
+    results = []  
+
+    for i in range(0, num_threads):
+        print("i:",  i)
+        start = i * chunk_size
+        end = start + chunk_size
+        print("start:",  start)
+        print("end:",  end)
+        if i == num_threads - 1:  end = num_numbers # Handle everything remaining in the last thread
+
+        thread = threading.Thread(target=wrapper_add_random_numbers, args=(start, end, results))
+        threads.append(thread)
+        thread.start()
+        print(f">> Starting {thread.name}")
+
+    for thread in threads:
+        thread.join()
+
+    # Calculate the total sum
+    total_sum = sum(results)
+    print(f">> Sum of random numbers: {total_sum}")
+
+    total_end_time = time.time()
     total_time = (total_end_time - total_start_time) * 1000
-    print(f"Threading case taken: {total_time} milliseconds")
+    print(f">> Time taken: {total_time} milliseconds")
+
     return total_time
