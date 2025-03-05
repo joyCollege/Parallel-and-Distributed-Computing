380fb5761a2d23750d1fd13d1addabea30452da4
Joy Anne Dela Cruz <60301959@udst.edu.qa>
2025-03-05 10:56:35 +0300

    Set-up new branch

--- main.py
+++
@@ -1,3 +0,0 @@
-from src.pt1_main import lab4_1
-
-lab4_1()
--- src/pt1_global_vars.py
+++
@@ -1,2 +0,0 @@
-latest_temperatures = {}  # To store the latest temperature reading
-temperature_averages = {}  # To store the average temperature
