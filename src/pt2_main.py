7c88865296dd58d813fe13cc309dab8b9d2de756
Joy Anne Dela Cruz <60301959@udst.edu.qa>
2025-03-05 10:39:21 +0300

    Updated hessian

--- src/pt2_main.py
+++ src/pt2_main.py
@@ -11,14 +11,18 @@ def lab4_2():
     # yes_images, no_images = yes_images[0:3], no_images[0:3] #comment out if you want a full run
     nonparallel_time = nonparallel_time - time()
 
-    from .pt2_process_image_sequential import sequential_run
-    sequential_time, yes_processed_sequential, no_processed_sequential = sequential_run(yes_images, no_images)
-    save_processed_images(yes_processed_sequential, "yes_full_processed_images.pkl")
-    save_processed_images(no_processed_sequential, "no_full_processed_images.pkl")
-    print()
+    # from .pt2_process_image_sequential import sequential_run
+    # sequential_time, yes_processed_sequential, no_processed_sequential = sequential_run(yes_images, no_images)
+    # save_processed_images(yes_processed_sequential, "yes_full_processed_images.pkl")
+    # save_processed_images(no_processed_sequential, "no_full_processed_images.pkl")
+    # print()
 
-    from .pt2_process_image_pooling import pooling_run
-    pooling_time, yes_processed_pooling, no_processed_pooling = pooling_run(yes_images, no_images)
+    # from .pt2_process_image_pooling import pooling_run
+    # pooling_time, yes_processed_pooling, no_processed_pooling = pooling_run(yes_images, no_images)
+    # calc_print_analysis(sequential_time, pooling_time, pooling_time/(pooling_time+nonparallel_time), "pooling process_image")
+
+    sequential_time = 9717.20770573616
+    pooling_time = 1908.0378203392029
     calc_print_analysis(sequential_time, pooling_time, pooling_time/(pooling_time+nonparallel_time), "pooling process_image")
 
     from .pt2_process_image_hessian import hessian_run
--- src/pt2_process_image_hessian.py
+++ src/pt2_process_image_hessian.py
@@ -12,31 +12,33 @@ def compute_hessian_for_sigma(args):
     image, sigma = args
     return hessian(image, sigmas=[sigma]) 
 
-def compute_hessian(image, sigmas):
-    with Pool(processes=None) as pool: 
-        results = pool.map(compute_hessian_for_sigma, [(image, s) for s in sigmas])
+# Only one compute_hessian function using a pool
+def compute_hessian(image, sigmas, pool):
+    results = pool.map(compute_hessian_for_sigma, [(image, s) for s in sigmas])
     return results
 
-def process_single_image(image, output_list, lock):
+# Create a global pool
+global_hessian_pool = Pool(processes=6)
+
+def process_single_image(image, output_list, lock, pool):
     filtered_images = {
         'Original': image,
         'Entropy': entropy(image, disk(2)),
         'Gaussian': nd.gaussian_filter(image, sigma=1),
         'Sobel': sobel(image),
         'Gabor': gabor(image, frequency=0.9)[1],
-        'Hessian': compute_hessian(image, sigmas=range(1, 100, 1)),  # Parallelized Hessian
+        'Hessian': compute_hessian(image, sigmas=range(1, 100, 1), pool=pool),
         'Prewitt': prewitt(image)
     }
-    
-    with lock:  
+    with lock:
         output_list.append(filtered_images)
 
-def process_images_threaded(images, output_list):
+def process_images_threaded(images, output_list, pool):
     threads = []
     lock = Lock()  # Ensure thread safety
 
     for image in tqdm(images):
-        thread = Thread(target=process_single_image, args=(image, output_list, lock))
+        thread = Thread(target=process_single_image, args=(image, output_list, lock, pool))
         threads.append(thread)
         thread.start()
 
@@ -50,19 +52,22 @@ def hessian_run(yes_images, no_images):
     yes_processed_hessian = []
     no_processed_hessian = []
 
-    # Start both sets of images in parallel using threads
-    t1 = Thread(target=process_images_threaded, args=(yes_images[0:20], yes_processed_hessian))
-    t2 = Thread(target=process_images_threaded, args=(no_images[0:20], no_processed_hessian))
+    # Start both sets of images in parallel using threads, passing the global pool
+    t1 = Thread(target=process_images_threaded, args=(yes_images, yes_processed_hessian, global_hessian_pool))
+    t2 = Thread(target=process_images_threaded, args=(no_images, no_processed_hessian, global_hessian_pool))
 
     t1.start()
     t2.start()
 
-    # Only one join per thread (no need for separate joins for each set)
     t1.join()  
     t2.join() 
 
     end_time = time()
     execution_time_hessian = end_time - start_time
+
+    # Properly close the pool after use
+    global_hessian_pool.close()
+    global_hessian_pool.join()
 
     print(f"Hessian execution time: {execution_time_hessian:.2f} seconds (combined threading and pooling)")
     return (execution_time_hessian, yes_processed_hessian, no_processed_hessian)
