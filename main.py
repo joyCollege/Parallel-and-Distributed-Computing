e9cd251e6fc0bf4bf6a74ad69e62ede78a22dc00
Joy Anne Dela Cruz <60301959@udst.edu.qa>
2025-03-09 11:42:54 +0300

    Added the square example

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
 from mpi4py import MPI
+import numpy as np
+from src.square import square
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
-if rank == 0:
-    # Process 0 sends a non-blocking message to Process 1
-    data_to_send = "Hello from Process 0"
-    request = comm.isend(data_to_send, dest=1, tag=100)
-    # Process 0 can perform other work here while the send operation completes
-    request.wait() # Wait for the non-blocking send to complete
-    print("Process 0 sent data: ", data_to_send)
-elif rank == 1:
-    # Process 1 sets up a non-blocking receive from Process 0
-    request = comm.irecv(source=0, tag=100)
-    data_received = request.wait() # Wait for the non-blocking receive to complete
-    # ’status’ object contains information about the received message
-    status = request.Get_status()
-    print("Process 1 received data: ", data_received)
-    print("Status of the received message: ", status)
+size = comm.Get_size() #number of processes 
 
-#mpiexec -n 2 python3 main.py
+print(f"rank: {rank} of {size}")
+
+if rank == 0:
+    numbers = np.arange(size, dtype = 'i')
+else:
+    numbers = None
+print("numbers", numbers)
+
+number = np.zeros(1, dtype = 'i')
+comm.Scatter(numbers, number, root =0) # MPI_Bcast() sends the same piece of data to everyone, while MPI_Scatter() sends each process a part of the input array.
+print("numbers:", numbers)
+print("number:", number)
+
+result = square(number[0])
+print(result)
+import time 
+import random
+
+time.sleep(random.random())
+request = comm.isend(result, dest=0, tag=rank)
+
+if rank == 0:
+    results = np.zeros(size, dtype = 'i')
+    for i in range(size):
+        results[i] = comm.irecv(source=i, tag=i).wait()
+    print("results", results)
+
+request.wait()
+
+    
+
+# mpirun -n 6 python main.py
