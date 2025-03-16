cba5cc0c514e6f75706c5b09cd0c3111c8b1a9a7
Joy Anne Dela Cruz <60301959@udst.edu.qa>
2025-03-16 13:36:52 +0300

    Wrote a README with conclusions and ran 10^6

--- main.py
+++ main.py
@@ -5,10 +5,36 @@ from time import time
 from src.connectionPool import ConnectionPool, access_database
 from multiprocessing import Process
 
-LIST_SIZE = 10**6
-NUM_PROCESSES = 600
+LIST_SIZE = 10**7
+NUM_PROCESSES = 700
 
 if __name__ == '__main__':
+    """
+    Main script to compare different multiprocessing approaches for squaring numbers and simulating database operations.
+
+    The script:
+    1. Generates a list of numbers up to LIST_SIZE.
+    2. Runs different multiprocessing techniques to square numbers, including:
+    - Sequential processing
+    - Multiprocessing with direct process creation
+    - Multiprocessing with Pool methods (map, apply, async)
+    - Multiprocessing with an Executor-based approach
+    3. Measures and compares execution times across these methods.
+    4. Simulates a database connection pool using semaphores, where multiple processes request and release connections.
+
+    Constants:
+        LIST_SIZE (int): The size of the list of numbers to be squared.
+        NUM_PROCESSES (int): The number of processes simulating database access.
+
+    Outputs:
+        - Execution times for each method.
+        - Last three squared numbers for validation.
+        - Performance analysis comparing different parallelization methods.
+        - Simulated database access log.
+
+    Note:
+        Creating too many processes (e.g., LIST_SIZE number of processes) may lead to memory errors.
+    """
     print(
 """
 ====================================================================================
@@ -21,14 +47,14 @@ SQUARE PROGRAMS USING MULTIPROCESSING AND CONCURRENT FUTURES
     sequential_time, sequential_list = sequential_run(num_list)
 
     from src.multiprocessing import multiprocessing_run
-    try:
-        multiprocessing_totalTime = time()
-        multiprocessing_time, multiprocessing_list = multiprocessing_run(num_list)
-        multiprocessing_totalTime = time() - multiprocessing_totalTime
-        multiprocessing_successful = True
-    except:
-        print(f"multiprocessing_run couldn'nt make {LIST_SIZE} processes")
-        multiprocessing_successful = False
+
+    # multiprocessing_totalTime = time()
+    # multiprocessing_time, multiprocessing_list = multiprocessing_run(num_list)
+    # multiprocessing_totalTime = time() - multiprocessing_totalTime
+    # multiprocessing_successful = True
+
+    print(f"********* multiprocessing_run couldn't make {LIST_SIZE} processes *********")
+    multiprocessing_successful = False
 
         
 
@@ -78,6 +104,5 @@ SIMULATED DATABASE OPERATION WITH SEMAPHORES
         processes.append(process)
         process.start()
     
-    # Wait for all processes to complete.
     for process in processes:
         process.join()
