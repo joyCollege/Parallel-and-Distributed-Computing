21b21ccebd0ed207d7c98221d10855330e435fb4
Joy Anne Dela Cruz <60301959@udst.edu.qa>
2025-03-19 23:24:12 +0300

    Remove gustafson and amdahl in calculating analysis

--- src/calc_print_analysis.py
+++ src/calc_print_analysis.py
@@ -1,26 +1,19 @@
 from src.calc_speedup import calc_speadup
 from src.calc_efficiency import calc_efficiency
-from src.calc_amdahl import calc_amdahl
-from src.calc_gustafson import calc_gustafson
 
-def calc_print_analysis(serial_time, parallel_time, parallelized_portion, title):
+def calc_print_analysis(serial_time, parallel_time, title):
     """
     Perform and print performance analysis based on speedup, efficiency, Amdahl’s speedup, and Gustafson’s speedup.
 
     :param num_actions: Number of processes or threads used in the parallel execution.
     :param serial_time: Time taken for the serial (single-threaded) execution.
     :param parallel_time: Time taken for the parallel execution.
-    :param parallelized_portion: The fraction of the workload that can be parallelized (between 0 and 1).
     :param title: The title to be printed at the start of the analysis (e.g., "Threading Performance Analysis").
     """
     print(f"\n**************** {title} Performance Analysis ****************")
     speedup = calc_speadup(serial_time, parallel_time)
     efficiency = calc_efficiency(speedup, 6)
-    amdahl_speedup = calc_amdahl(6, parallelized_portion)
-    gustafson_speedup = calc_gustafson(6, 1 - parallelized_portion)
     
     # Print the results
     print(f"Speedup             {speedup:.16f}")
     print(f"Efficiency          {efficiency:.16f}")
-    print(f"Amdahl’s speedup    {amdahl_speedup:.16f}")
-    print(f"Gustafson’s speedup {gustafson_speedup:.16f}")
